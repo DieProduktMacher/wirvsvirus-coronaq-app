f53327ffd3d45278b27633c78d1793cfb31cbab7
Thomas Limp <thomas.limp@produktmacher.com>
2020-03-22 07:25:56 +0100

    refactor footer and main

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,33 +1,24 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Router from "./Router";
+
 import "./App.css";
 import { ThemeProvider } from "@material-ui/styles";
 import createTheme from "./createTheme";
+
 import { ContextProvider } from "./Context";
 
 import PageHeader from "../PageHeader/PageHeader";
+import PageMain from "../PageMain/PageMain";
+import PageFooter from "../PageFooter/PageFooter";
 
 function App() {
   return (
     <ThemeProvider theme={createTheme()}>
       <ContextProvider>
         <BrowserRouter>
-          <div className="page-container">
-            <PageHeader />
-            <main className="page-main">
-              <Router />
-            </main>
-            <footer className="page-footer">
-              <a href="https://wirvsvirushackathon.org/">
-                <img
-                  src="https://wirvsvirushackathon.org/wp-content/uploads/2020/03/12-scaled.jpg"
-                  alt="Initiiert durch WirVsVirus"
-                  className="page-footer__logo-wirvsvirus"
-                />
-              </a>
-            </footer>
-          </div>
+          <PageHeader />
+          <PageMain />
+          <PageFooter />
         </BrowserRouter>
       </ContextProvider>
     </ThemeProvider>
