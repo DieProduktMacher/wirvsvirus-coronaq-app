a61ea559090151d5156b00cfd3beb79255d19c70
Thomas Limp <thomas.limp@produktmacher.com>
2020-03-30 09:32:53 +0200

    add basic analytics integration possibilities

---
+++ src/components/App/Analytics.ts
@@ -0,0 +1,7 @@
+class Analytics {
+  page(path: string) {
+    console.log(`[Analyics] page ${path}`);
+  }
+}
+
+export default new Analytics();
--- src/components/App/Router.tsx
+++ src/components/App/Router.tsx
@@ -1,15 +1,28 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Switch, withRouter } from "react-router-dom";
 import { root, steps } from "./Routes";
+import analytics from "./Analytics";
+
+const Logger = withRouter(({ location }) => {
+  const pathname = location.pathname;
+  useEffect(() => {
+    analytics.page(pathname);
+  }, [pathname]);
+
+  return null;
+});
 
 const Router = () => {
   return (
-    <Switch>
-      {steps.map(({ path, component }) => (
-        <Route path={path} component={component} key={path} />
-      ))}
-      <Route path="/" component={root.component} />
-    </Switch>
+    <>
+      <Logger />
+      <Switch>
+        {steps.map(({ path, component }) => (
+          <Route path={path} component={component} key={path} />
+        ))}
+        <Route path="/" component={root.component} />
+      </Switch>
+    </>
   );
 };
 
